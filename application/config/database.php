5e49a28f896f33325b93541b4b5cdda52550e79c
Russ Zeien <russ@zeien.org>
2012-12-21 22:00:45 -0500

    mvc test db account

--- application/config/database.php
+++ application/config/database.php
@@ -3,7 +3,7 @@
 
 $config['type'] 		= 'mysql';
 $config['hostname'] 	= 'localhost';
-$config['port']			= null;
+$config['port'] 		= null;
 $config['username']		= 'mvctest';
 $config['password']		= 'mvctest123';
 $config['database']		= 'checkip';
