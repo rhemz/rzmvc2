22cecdbdec4a1f75584af863937d0d4d3f154654
Russ Zeien <russ@zeien.org>
2013-01-02 00:01:42 -0500

    use environment-aware logging

--- framework/classes/database_interface.php
+++ framework/classes/database_interface.php
@@ -20,5 +20,14 @@ interface Database_Interface
 
 class Database_Connection_Exception extends Rz_MVC_Exception
 {
-	public function __construct($msg) { parent::__construct($msg); }
+	public function __construct($type, $host, $port, $user, $pass)
+	{ 
+		$message = sprintf("Unable to connect to %s server ", $type);
+
+		$message .= ENVIRONMENT == Environment::Development
+			? sprintf("(%s) on port %d with credentials %s/%s", $host, $port, $user, $pass)
+			: "using the supplied settings";
+
+		parent::__construct($message);
+	}
 }
