1efaf9ca9109ca6fb69fab64e26701b0eff112b4
Russ Zeien <russ@zeien.org>
2013-01-07 18:17:09 -0500

    update default framework logging config

--- framework/config/logging.php
+++ framework/config/logging.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['write_file']  = true;
+$config['write_file']	= true;
 $config['print']		= Log_Level::Warning | Log_Level::Error;
 $config['print_html']	= true;
 $config['log_level']	= Log_Level::Error;
