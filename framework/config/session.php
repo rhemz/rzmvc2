66b245de7482307456f27f4037ea92df8f468e49
Russ Zeien <russ@zeien.org>
2013-01-16 18:19:57 -0500

    removing file prefix

--- framework/config/session.php
+++ framework/config/session.php
@@ -3,6 +3,4 @@
 $config['type']			= 'file';
 $config['name']			= 'rzmvc';
 $config['timeout']		= 60*60*24; // 1 day
-
-$config['file_prefix']	= 'mvcsession_';
 $config['hash']			= 'jksOQauDP2TrCE10JvPJlsXaOKREv';
