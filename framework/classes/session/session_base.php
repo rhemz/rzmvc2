a2ad236a036a6f16153511fd8f4678ba43263d79
Russ Zeien <russ@zeien.org>
2013-01-17 16:09:19 -0500

    renamed from base_session

---
+++ framework/classes/session/session_base.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+	Ideas:
+	http://www.php.net/manual/en/function.session-set-save-handler.php
+	https://github.com/fuel/core/blob/1.5/develop/classes/session/driver.php
+	http://www.php.net/session_regenerate_id
+	http://stackoverflow.com/questions/11596082/php-session-class-similar-to-codeigniter-session-class
+*/
+
+abstract class Session_Base
+{
+	protected $data = array();
+	protected $config = array();
+	protected $timestamp = null;
+	protected $started = false;
+
+
+	abstract public function _open();
+
+	abstract public function _close();
+
+	abstract public function _read($id);
+
+	abstract public function _write($id, $data);
+
+	abstract public function _destroy($id);
+
+	abstract public function _gc();
+
+	abstract public function set($key, $data);
+
+	abstract public function get($key, $default = null);
+
+
+	protected function start()
+	{
+		session_name(Config::get_instance()->get('session.name'));
+
+		session_start();
+	}
+
+
+	protected function _set_session_handler()
+	{
+		session_set_save_handler(
+			array($this, '_open'), 
+			array($this, '_close'),
+			array($this, '_read'),
+			array($this, '_write'),
+			array($this, '_destroy'),
+			array($this, '_gc')
+		);
+	}
+
+
+	protected function _generate_key()
+	{
+		$key = Config::get_instance()->get('session.hash');
+
+		return sha1(microtime(true) . $key);
+	}
+
+
+
+}
