8d376f13ed70fe330cef600485c3159e95d02b2c
Russ Zeien <russ@zeien.org>
2013-01-25 20:36:32 -0500

    commenting

--- framework/classes/database/result_set.php
+++ framework/classes/database/result_set.php
@@ -1,6 +1,9 @@
 <?php
 
-
+/**
+* Generic database resultset object.  Provides a simple container for any number of rows returned
+* by a database query in object form.
+*/
 class Result_Set
 {
 	public $rows = array();
@@ -11,7 +14,7 @@ class Result_Set
 		// make selected column names and their values accessible as instance variables
 		foreach($data as $row)
 		{
-			$row_obj = new stdClass();
+			$row_obj = new stdClass();  // use php's included stdclass
 			
 			foreach($row as $key => $value)
 			{
