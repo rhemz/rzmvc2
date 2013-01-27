4e176bc3fe09154cb1c05c2a47740d03f1e050fd
Russ Zeien <russ@zeien.org>
2013-01-26 19:41:53 -0500

    new REST controller to aid in implementing RESTful services

---
+++ framework/classes/controller_rest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+/**
+* Controller specifically for implementing RESTful services.
+* REST controller inherits from Controller and is responsible for providing information for URI routing.
+*/
+class Controller_Rest extends Rz_Mvc
+{
+	private $method_prefix;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->method_prefix = sprintf("%s_", Input::request_method());
+	}
+
+	
+	/**
+	* Used by the router to determine if a given controller maps to a URI
+	* @param string $method Method to test for
+	*/
+	public function _has_method($method)
+	{
+		$rc = new ReflectionClass($this);
+
+		// find function by http method
+		return $rc->hasMethod(sprintf("%s%s", $this->method_prefix, $method));
+	}
+
+
+	public function __call($method, $args)
+	{
+		$method = sprintf("%s%s", $this->method_prefix, $method);
+
+		sizeof($args)
+			? call_user_func_array(array($this, $method), $args)
+			: $this->{$method}();
+	}
+}
