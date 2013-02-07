3a6419db609e6a87b6ba9e25bc2a4b216601f258
Russ Zeien <russ@zeien.org>
2013-02-06 23:30:51 -0500

    adding session database config

--- framework/config/session.php
+++ framework/config/session.php
@@ -1,6 +1,9 @@
 <?php
 
-$config['type']			= 'file';
+$config['type']			= 'database';
 $config['name']			= 'rzmvc';
 $config['timeout']		= 60*60*24; // 1 day
 $config['hash']			= 'jksOQauDP2TrCE10JvPJlsXaOKREv';
+
+// for database sessions
+$config['table_name']	= 'mvcsession'; 
