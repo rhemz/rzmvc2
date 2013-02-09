15a9a85927f1fe4d492450dfe1985a1c31bcfdd0
Russ Zeien <russ@zeien.org>
2013-02-08 21:10:35 -0500

    changing default session handler to file based

--- framework/config/session.php
+++ framework/config/session.php
@@ -2,7 +2,7 @@
 
 $config['use_session']	= true;
 
-$config['type']			= 'database';
+$config['type']			= 'file'; // currently supported are 'file', 'database'
 $config['name']			= 'rzmvc';
 $config['timeout']		= 60*60*24; // 1 day
 $config['hash']			= "9C]G#tx6M^X'=x>l[6v(zj:%FrcA6*e}"; // change this in your application settings!
