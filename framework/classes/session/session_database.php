a8baa270a9dc7e769fd45b6d2be915008be0f852
Russ Zeien <russ@zeien.org>
2013-02-08 20:35:43 -0500

    adding encryption functionality

--- framework/classes/session/session_database.php
+++ framework/classes/session/session_database.php
@@ -3,8 +3,8 @@
 /*
 CREATE TABLE `mvcsession` ( 
 	`id` varchar(32) NOT NULL, 
-	`contents` blob, 
-	`modify_date` datetime NOT NULL, 
+	`contents` longtext, 
+	`modify_date` int NOT NULL,
 PRIMARY KEY (`id`),
 KEY `modify_date` (`modify_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -12,12 +12,31 @@ KEY `modify_date` (`modify_date`)
 
 class Session_Database extends Session_Base
 {
-	private $model;
+	const Cipher = 'rijndael-256';
+	const Cipher_Mode = 'cbc';
 
+	private $model;
+	private $crypt;
+	private $iv;
+	private $iv_len;
+	
 
 	public function __construct()
 	{
-		$this->config = $this->config = Config::get_instance()->get('session.*');
+		$this->config = Config::get_instance()->get('session.*');
+
+		if($this->config['encrypt'])
+		{
+			$this->iv_len = mcrypt_get_iv_size(self::Cipher, self::Cipher_Mode);
+
+			if(strlen($this->config['hash']) != $this->iv_len)
+			{
+				Logger::log(sprintf('The session hash length must be exactly %d characters', self::Cipher_Length), Log_Level::Error);
+			}
+
+			$this->iv = mcrypt_create_iv($this->iv_len, MCRYPT_RAND);
+		}
+
 		$this->_set_session_handler();
 		$this->start();
 	}
@@ -28,8 +47,11 @@ class Session_Database extends Session_Base
 		$this->model = new Rz_MVC_Session_Model($this->config['table_name']);
 
 		// garbage collect old sessions
-		$this->_gc($this->config['timeout']);
-
+		if(rand(0, 100) <= $this->config['gc_percent'])
+		{
+			$this->_gc($this->config['timeout']);
+		}
+		
 		return true;
 	}
 
@@ -42,12 +64,25 @@ class Session_Database extends Session_Base
 
 	public function _read($id)
 	{
-		return $this->model->get_session_data($id);
+		$data = $this->model->get_session_data($id);
+		
+		// Logger::log('read data: ' . $data);
+		
+		return $this->config['encrypt']
+			? $this->decrypt($data)
+			: $data;
 	}
 
 
 	public function _write($id, $data)
 	{
+		// Logger::log('write data: ' . $data);
+		$data = $this->config['encrypt']
+			? $this->encrypt($data)
+			: $data;
+
+		// Logger::log('write data: ' . $data);
+
 		return $this->model->save_session_data($id, $data);
 	}
 
@@ -64,19 +99,31 @@ class Session_Database extends Session_Base
 	}
 
 
-	public function set($key, $data)
+	private function get_lock_name()
 	{
-		$_SESSION[$key] = $data;
-		return true;
+		return 'sessionlock' . session_id();
 	}
 
 
-	public function get($key, $default = null)
+	private function encrypt($data)
 	{
-		return isset($_SESSION[$key])
-			? $_SESSION[$key]
-			: $default;
+		return strlen($data) > 0
+			? base64_encode(mcrypt_encrypt(self::Cipher, $this->config['hash'], $data, self::Cipher_Mode))
+			: null;
 	}
+
+
+	private function decrypt($data)
+	{
+		return base64_decode(mcrypt_decrypt(self::Cipher, $this->config['hash'], $data, self::Cipher_Mode));
+	}
+
+
+	// public function __destruct()
+	// {
+	// 	mcrypt_generic_deinit($this->crypt);
+	// 	mcrypt_module_close($this->crypt);
+	// }
 
 }
 
@@ -116,11 +163,11 @@ class Rz_MVC_Session_Model extends Model
 	{
 		if($this->get_session_data($id) !== false)
 		{
-			return $this->query("UPDATE {$this->table} SET contents = ?, modify_date=now() WHERE id = ?", array($data, $id));
+			return $this->query("UPDATE {$this->table} SET contents = ?, modify_date=? WHERE id = ?", array($data, time(), $id));
 		}
 		else
 		{
-			return $this->query("INSERT INTO {$this->table} (id, contents, modify_date) VALUES (?, ?, now())", array($id, $data));
+			return $this->query("INSERT INTO {$this->table} (id, contents, modify_date) VALUES (?, ?, ?)", array($id, $data, time()));
 		}
 	}
 
@@ -133,7 +180,7 @@ class Rz_MVC_Session_Model extends Model
 
 	public function garbage_collect($age)
 	{
-		// return $this->query("DELETE FROM {$this->table} WHERE (now() - INTERVAL ? SECOND)", array($age));
+		return $this->query("DELETE FROM {$this->table} WHERE modify_date < ?", array((time() - $age)));
 	}
 
 }
