dfd6ac7bd47364c19cbece37b9c80c154d98aa14
Russ Zeien <russ@zeien.org>
2013-02-14 15:25:22 -0500

    moved log filenames into logging config

--- framework/config/logging.php
+++ framework/config/logging.php
@@ -4,3 +4,7 @@ $config['write_file']	= true;
 $config['print']		= Log_Level::Warning | Log_Level::Error;
 $config['print_html']	= true;
 $config['log_level']	= Log_Level::Error;
+
+$config['notice_path']	= 'notices.log';
+$config['warning_path']	= 'warnings.log';
+$config['error_path']	= 'errors.log';
