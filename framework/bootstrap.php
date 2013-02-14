4eec2105e1b7b7115d767f66717324dcb923025d
Russ Zeien <russ@zeien.org>
2013-02-14 15:23:33 -0500

    manually loading a few classes rather than simply autoloader to allow auto to use config

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -1,24 +1,31 @@
 <?php
 
+// load base mvc exception, configuration class, and autoloader
+require_once(FRAMEWORK_PATH . 'classes' . DIRECTORY_SEPARATOR . 'rz_mvc_exception' . PHP_EXT);
+require_once(FRAMEWORK_PATH . 'classes' . DIRECTORY_SEPARATOR . 'config' . PHP_EXT);
 require_once(FRAMEWORK_PATH . 'autoload' . PHP_EXT);
 
+// load core framework and application configs
 $config =& Config::get_instance();
-$config->load(array('global', 'environment', 'logging', 'session'));
+$config->load(array('paths', 'routes', 'global', 'environment', 'logging', 'session'));
 
+// register custom shutdown and error handler hooks
 if($config->get('global.framework_handle_fatal_errors'))
 {
-	// register custom shutdown and error handler hooks
 	register_shutdown_function('Rz_MVC::hook_shutdown');
 	ini_set('display_errors', 0);
 }
 
+// check for existance of application environment config file
 if(!$config->user_config_exists('environment'))
 {
 	Logger::log('No application environment setting present, falling back to framework default', Log_Level::Warning);
 }
 
+// set operating environment
 define('ENVIRONMENT', $config->get('environment.environment'));
 
+// prepare URI and startup Router
 $uri = rtrim(preg_replace('/\?(.*)/', '', $_SERVER['REQUEST_URI']), '/');
 
 $router = new Router($uri);
