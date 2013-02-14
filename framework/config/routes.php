c7949c526d66a215534b09880487319b0f6cc6dc
Russ Zeien <russ@zeien.org>
2013-02-14 15:25:52 -0500

    removed class suffixes from routes.  moved to paths

--- framework/config/routes.php
+++ framework/config/routes.php
@@ -10,7 +10,4 @@ $config['default_function'] = 'index';
 
 $config['404_view'] = '404';
 
-$config['controller_suffix'] = 'controller';
-$config['model_suffix'] = 'model';
-
 $config['wildcard'] = '%var';
