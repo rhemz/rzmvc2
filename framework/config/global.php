d10fa5407bf2b0c9e286983c36818f13857a2cb0
Russ Zeien <russ@zeien.org>
2013-02-14 18:44:07 -0500

    add smart loading config option

--- framework/config/global.php
+++ framework/config/global.php
@@ -1,4 +1,7 @@
 <?php
 
+// turning this on will allow you to make config queries without manually loading beforehand
+$config['smart_config_loading']				= false;
 
-$config['framework_handle_fatal_errors']		= true;
+// catch FATAL PHP errors and intelligent display/log them based on environment
+$config['framework_handle_fatal_errors']	= true;
