3f3699cf540db8d3a686f377ddfaf402e3323769
Russ Zeien <russ@zeien.org>
2013-08-01 11:22:30 -0400

    removing application, which is now it's own repo

--- application/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$config['type'] 		= null;
-$config['hostname'] 	= 'localhost';
-$config['port'] 		= null;
-$config['username']		= '';
-$config['password']		= '';
-$config['database']		= '';
--- application/config/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config['use_session']	= true;
-
-$config['type']			= 'file';
-$config['name']			= 'my_rzmvc_app';
